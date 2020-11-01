faf91ccb7944aae294c3a4ce06283b1cf4657a99
chenf24k <chenf24k@163.com>
2020-11-01 19:48:35 +0800

    .

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 
-const h1 = React.createElement('h1', null, '这里是h1');
-const div = React.createElement('div', { id: "first" }, '这里是div', h1);
+// const h1 = React.createElement('h1', null, '这里是h1');
+// const div = React.createElement('div', { id: "first" }, '这里是div', h1);
 
-const divtest = <div title="123">1123</div>;
+/**
+ * JSX，是一个 JavaScript 的语法扩展。
+ * 我们建议在 React 中配合使用 JSX，JSX 可以很好地描述 UI 应该呈现出它应有交互的本质形式。
+ * JSX 可能会使人联想到模版语言，但它具有 JavaScript 的全部功能。
+ */
 
-ReactDOM.render(divtest, document.getElementById('root'));
+const name = 'Josh Perez';
+const element = <h1>Hello,{name}</h1>;
+
+ReactDOM.render(element, document.getElementById('root'));
